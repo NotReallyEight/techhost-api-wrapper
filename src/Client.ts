a549c4e6640559d963c2824eaccb0143ee64a312
NotReallyEight <wangzndg@gmail.com>
2022-01-10 21:45:48 +0100

    Add dog endpoint

--- src/Client.ts
+++ src/Client.ts
@@ -1,5 +1,6 @@
 import { Rest } from ".";
 import { Cat } from "./structures/Cat";
+import { Dog } from "./structures/Dog";
 
 /**
  * The client class
@@ -14,8 +15,21 @@ export class Client {
 		this.rest = new Rest(this);
 	}
 
+	/**
+	 * Get a url of a cat image
+	 * @returns - The Cat class
+	 */
 	async cat(): Promise<Cat | null> {
 		const cat: string | null = await this.rest.request("/cat", "GET");
 		return cat != null ? new Cat(cat) : null;
+	}
+
+	/**
+	 * Get a url of a dog image
+	 * @returns - The Dog class
+	 */
+	async dog(): Promise<Dog | null> {
+		const dog: string | null = await this.rest.request("/dog", "GET");
+		return dog != null ? new Dog(dog) : null;
 	}
 }
