0a0fbcf30786a9679202ea767fb7f0909c701000
NotReallyEight <wangzndg@gmail.com>
2022-01-10 21:56:56 +0100

    Add fact endpoint

--- src/Client.ts
+++ src/Client.ts
@@ -1,6 +1,7 @@
 import { Rest } from ".";
 import { Cat } from "./structures/Cat";
 import { Dog } from "./structures/Dog";
+import { Fact } from "./structures/Fact";
 
 /**
  * The client class
@@ -31,5 +32,14 @@ export class Client {
 	async dog(): Promise<Dog | null> {
 		const dog: string | null = await this.rest.request("/dog", "GET");
 		return dog != null ? new Dog(dog) : null;
+	}
+
+	/**
+	 * Get a random fact
+	 * @returns - The Fact class
+	 */
+	async fact(): Promise<Fact | null> {
+		const fact: string | null = await this.rest.request("/fact", "GET");
+		return fact != null ? new Fact(fact) : null;
 	}
 }
